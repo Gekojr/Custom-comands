56c98dc83804fd0c5faa39b236576214ac3e21ec
Gekojr <lorenzo.delgreco@levicivita.edu.it>
2026-09-25 15:35:37 +0200

    Add src/main/java/com/gekojr/customcomands/CustomComands.java

---
+++ src/main/java/com/gekojr/customcomands/CustomComands.java
@@ -0,0 +1,62 @@
+package com.gekojr.customcomands;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomComands extends JavaPlugin implements CommandExecutor {
+
+    private static final String DISCORD = "https://discord.gg/mZRyBRzSA3";
+
+    @Override
+    public void onEnable() {
+        getCommand("discord").setExecutor(this);
+        getCommand("annuncio").setExecutor(this);
+        getLogger().info("CustomComands attivo.");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("CustomComands disattivato.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("discord")) {
+            sender.sendMessage(
+                Component.text("Discord: ", NamedTextColor.AQUA)
+                    .append(Component.text(DISCORD, NamedTextColor.WHITE))
+            );
+            return true;
+        }
+
+        if (command.getName().equalsIgnoreCase("annuncio")) {
+            if (!sender.hasPermission("customcomands.annuncio")) {
+                sender.sendMessage(Component.text("Non hai il permesso per usare questo comando.", NamedTextColor.RED));
+                return true;
+            }
+
+            if (args.length == 0) {
+                sender.sendMessage(Component.text("Uso: /annuncio <messaggio>", NamedTextColor.RED));
+                return true;
+            }
+
+            String message = String.join(" ", args);
+
+            Component announcement = Component.text()
+                .append(Component.text("ANNUNCIO", NamedTextColor.GOLD, TextDecoration.BOLD))
+                .append(Component.text(" » ", NamedTextColor.DARK_GRAY))
+                .append(Component.text(message, NamedTextColor.WHITE))
+                .build();
+
+            getServer().sendMessage(announcement);
+            return true;
+        }
+
+        return false;
+    }
+}
